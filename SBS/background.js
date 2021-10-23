5d398d677a2fb528d098d41fc5edfd9c320b2a97
Norbert Waszkowiak <n.waszkowiak@sages.com.pl>
2021-10-23 18:33:41 +0200

    this is final

--- SBS/background.js
+++ SBS/background.js
@@ -1,7 +1,9 @@
-realApiKey = 'vj103o0ndh19vkea0mo8hnx2zbxigti1yhp2st3yqxqpmbp0o974vvsnsyurfwol'
+realApiKey = 'lnhhnu0apwer7yqeca6t6ow1msux2jq4ffplppdnn9fggm0jn411vbb96g643zqb'
+
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     console.log(tab.url);
+
     isSafeUrl(tab.url).then((value) => {
         if (value) {
             console.log("Safe page");
@@ -11,14 +13,38 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             replaceTab(tab, 3)
         }
     })
+
 });
+
+async function isSafeUrl(url) {
+    if(isFromUncheckedList(url)){
+        return true;
+    }
+    if(isFromLastUpdate(url)){
+        return lastResult
+    }
+    lastResult = await ifApiCorrect(url)
+    lastCheckingUrl = url
+    return lastResult
+
+}
+
+function isFromUncheckedList(url){
+    return url.startsWith("https://google.com//") || url.startsWith("chrome-extension://") || url.startsWith("chrome:")
+}
+
+var lastCheckingUrl
+var lastResult = true
+function isFromLastUpdate(url) {
+    return url == lastCheckingUrl
+}
 
 function replaceTab(tab, tryies) {
     chrome.tabs.get(tab.id)
         .then((tab) => {
-
             chrome.tabs.update(tab.id, { url: chrome.runtime.getURL('Blocked/index.html') })
                 .then((val) => {
+                    checkingTabID = val
                     console.log(val)
                 })
                 .catch((e) => {
@@ -48,12 +74,10 @@ function replaceTab(tab, tryies) {
 //<script type="module" src="milsymbol-2.0.0/src/milsymbol.js"></script>
 
 
-async function isSafeUrl(url) {
-    return ifApiCorrect(url)
-}
+
 
 async function handleScanResult(whichJob) {
-    const url = 'https://developers.checkphish.ai/api/neo/scan';
+    const url = 'https://developers.checkphish.ai/api/neo/scan/status';
     const options = {
         method: 'POST',
         body: JSON.stringify({
